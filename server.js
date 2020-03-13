70911e56dc16ced028af71e25ca3a40c88217ab4
Alex <alexcouts18@gmail.com>
2020-03-13 14:49:31 -0400

    completed assignment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -11,15 +12,22 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
+//START_ASYNC -- Synchronous hash and response of password hashing.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    })
+  })
 
 //END_ASYNC
 
-//START_SYNC
+//START_SYNC -- synchronous hashing and response of the password
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+console.log(hash)
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash)
+console.log(result)
 
 //END_SYNC
 
